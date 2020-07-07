b781ea48cdb4cbea606fdd8ad960f5fad721a711
Marshall Sorenson <marshall@conceptsol.com>
2020-07-07 13:55:09 -0400

    Add logging to email send

--- packages/nestjs-email/src/email.service.ts
+++ packages/nestjs-email/src/email.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { EmailOptions } from './interfaces/email-options.interface';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private logger: Logger,
+    private readonly mailerService: MailerService
+  ) {
+    this.logger.setContext(EmailService.name);
+  }
 
   public async sendEmail(dto: EmailOptions): Promise<void> {
-    await this.mailerService.sendMail(dto);
+    try {
+      await this.mailerService.sendMail(dto);
+    } catch (e) {
+      // log the original error
+      this.logger.error(e.message, e.stack);
+      // throw a more friendly error
+      throw new InternalServerErrorException(
+        'Fatal error while trying to send email.'
+      );
+    }
   }
 }
