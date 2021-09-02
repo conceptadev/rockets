0ecca9d9b8aae0f979528338aba4c126d9ed6b0f
Thiago Ramalho <thiago.nramalho@gmail.com>
2021-09-02 12:32:55 -0300

    Exception filter unit tests

--- packages/nestjs-logging/src/logger-exception.filter.spec.ts
+++ packages/nestjs-logging/src/logger-exception.filter.spec.ts
@@ -1,28 +1,38 @@
-import { Test } from '@nestjs/testing';
+import { mock } from 'jest-mock-extended';
+
+import { ArgumentsHost, HttpServer } from '@nestjs/common';
 
 import { LoggerExceptionFilter } from './logger-exception.filter';
 import { LoggerService } from './logger.service';
 
 describe('LoggerExceptionFilter', () => {
+  let httpServer : HttpServer;
+  let loggerService : LoggerService;
+  let argumentsHost : ArgumentsHost;
   let loggerExceptionFilter: LoggerExceptionFilter;
-
+  let spyLoggerServiceException: any;
+  
   beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
-      providers: [
-        LoggerExceptionFilter,
-        {
-          provide: LoggerService,
-          useValue: {},
-        }
-      ],
-    }).compile();
-    
-    loggerExceptionFilter = moduleRef.get<LoggerExceptionFilter>(LoggerExceptionFilter);
+    httpServer = mock<HttpServer>();
+    loggerService = mock<LoggerService>();
+    argumentsHost = mock<ArgumentsHost>();
+    spyLoggerServiceException = jest.spyOn(loggerService, 'exception');
   });
 
   describe('IsDefined', () => {
     it('was LoggerExceptionFilter defined', async () => {
-      expect(loggerExceptionFilter).toBeDefined();
+      expect(httpServer).toBeDefined();
+      expect(loggerService).toBeDefined();
+      expect(argumentsHost).toBeDefined();
     });
   });
+
+  it('LoggerExceptionFilter.catch', async () => {
+    loggerExceptionFilter = new LoggerExceptionFilter(loggerService, httpServer);
+    loggerExceptionFilter.catch(new Error() , argumentsHost);
+    
+    expect(loggerExceptionFilter).toBeDefined();
+    expect(spyLoggerServiceException).toBeCalledTimes(1);
+  });
+  
 });
--- packages/nestjs-logging/src/logger-exception.filter.ts
+++ packages/nestjs-logging/src/logger-exception.filter.ts
@@ -1,16 +1,24 @@
-import { ArgumentsHost, Catch, Inject, Injectable } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
+import { ArgumentsHost, Catch, HttpServer, Inject, Injectable } from '@nestjs/common';
+import { BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core';
+import { threadId } from 'worker_threads';
 
 import { LoggerService } from './logger.service';
 
 @Catch()
 @Injectable()
 export class LoggerExceptionFilter extends BaseExceptionFilter {
-  /**
-   * Inject logger service
-   */
-  @Inject(LoggerService)
-  private loggerService!: LoggerService;
+
+  loggerService: LoggerService;
+
+  constructor (
+    @Inject(LoggerService)
+    loggerService: LoggerService,
+    @Inject(HttpAdapterHost)
+    applicationRef?: HttpServer
+  ) {
+    super(applicationRef);
+    this.loggerService = loggerService;
+  }
 
   /**
    * Override catch to log before returning exception.
