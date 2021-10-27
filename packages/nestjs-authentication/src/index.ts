a3903cb09122e345c53eee9fb8873cf1752cc921
Thiago Ramalho <thiago.nramalho@gmail.com>
2021-10-27 18:26:33 -0300

    update index

--- packages/nestjs-authentication/src/index.ts
+++ packages/nestjs-authentication/src/index.ts
@@ -1,3 +1,15 @@
-// export * from './services/password-creation.service';
-// export * from './services/password-storage.service';
-// export * from './services/password-strength.service';
+export * from './authentication.module';
+export * from './sign.controller';
+
+export * from './services/password-creation.service';
+export * from './services/password-storage.service';
+export * from './services/password-strength.service';
+
+export * from './interface/authentication-config-options.interface';
+export * from './interface/authentication-options.interface';
+export * from './interface/dto/credential-lookup.interface';
+export * from './interface/dto/authentication-response.interface';
+export * from './interface/dto/access-token.interface';
+export * from './interface/dto/signin.dto.interface';
+
+export * from './interface/service/credential-lookup.service.interface';
