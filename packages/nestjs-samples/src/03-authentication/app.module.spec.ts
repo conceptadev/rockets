e52ece48dc2cfccbbb779a0d38bb615c4e5326b4
Marshall Sorenson <marshall@conceptsol.com>
2021-12-01 10:31:52 -0500

    Remove controller test, no longer exists.

--- packages/nestjs-samples/src/03-authentication/app.module.spec.ts
+++ packages/nestjs-samples/src/03-authentication/app.module.spec.ts
@@ -1,37 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppModule } from './app.module';
-import {
-  AuthenticationController,
-  AuthenticationStrategyLocalInterface,
-} from '@rockts-org/nestjs-authentication';
 
 describe('AppModule', () => {
-  let authController: AuthenticationController;
-
-  beforeEach(async () => {
+  it('should be imported', async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    authController = module.get<AuthenticationController>(
-      AuthenticationController,
-    );
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe('listening', () => {
-    it('Authenticate', async () => {
-      const sign: AuthenticationStrategyLocalInterface = {
-        username: 'first_user',
-        password: 'AS12378',
-      };
-      const response = await authController.authenticate(sign);
-
-      expect(response.accessToken).toBeDefined();
-      expect(response.username).toBe(sign.username);
-    });
+    expect(module).toBeInstanceOf(TestingModule);
   });
 });
