cc50bd2795fa5edb12be6c7a422bc9a33246788b
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2021-12-02 15:54:09 -0300

    update acl module spec

--- packages/nestjs-access-control/src/access-control.module.spec.ts
+++ packages/nestjs-access-control/src/access-control.module.spec.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AccessControl } from 'accesscontrol';
+import { AccessControlDefaultService } from './access-control-default.service';
 import { AccessControlModule } from './access-control.module';
 import { AccessControlModuleOptions } from './interfaces/access-control-module-options.interface';
 import { AccessControlService } from './interfaces/access-control-service.interface';
@@ -35,6 +36,60 @@ describe('AccessControlModule', () => {
       const testService = moduleRef.get<TestService>(TestService);
 
       expect(testService).toBeInstanceOf(TestService);
+    });
+  });
+
+  describe('default', () => {
+    it('should import the dynamic module synchronously', async () => {
+      const moduleRef = await Test.createTestingModule({
+        imports: [AccessControlModule],
+      }).compile();
+
+      const accessModuleService = moduleRef.get<AccessControlDefaultService>(
+        AccessControlDefaultService,
+      );
+      expect(accessModuleService).toBeInstanceOf(AccessControlDefaultService);
+    });
+  });
+
+  describe('forRoot', () => {
+    it('should import the dynamic module synchronously', async () => {
+      const moduleRef = await Test.createTestingModule({
+        imports: [AccessControlModule.forRoot(configPlain)],
+      }).compile();
+
+      const accessModuleService = moduleRef.get<AccessControlDefaultService>(
+        AccessControlDefaultService,
+      );
+      expect(accessModuleService).toBeInstanceOf(AccessControlDefaultService);
+    });
+  });
+
+  describe('forRootAsync (no injection)', () => {
+    it('should import the dynamic module asynchronously', async () => {
+      const moduleRef = await Test.createTestingModule({
+        imports: [
+          AccessControlModule.forRootAsync({
+            useFactory: async () => {
+              return configPlain;
+            },
+          }),
+        ],
+      }).compile();
+
+      const accessModuleService = moduleRef.get<AccessControlDefaultService>(
+        AccessControlDefaultService,
+      );
+      expect(accessModuleService).toBeInstanceOf(AccessControlDefaultService);
+    });
+  });
+
+  //TODO: check
+  describe('forRootAsync (with injection)', () => {
+    it('should import the dynamic module asynchronously', async () => {
+      const moduleRef = await Test.createTestingModule({
+        imports: [],
+      }).compile();
     });
   });
 });
