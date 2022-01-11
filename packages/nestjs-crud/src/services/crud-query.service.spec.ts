23da704f7371281ee3055ca832dcd036c8b2d8ed
Priscilla Ribeiro <prsc.ribeiro@gmail.com>
2022-01-11 12:46:56 -0300

    create mocks

--- packages/nestjs-crud/src/services/crud-query.service.spec.ts
+++ packages/nestjs-crud/src/services/crud-query.service.spec.ts
@@ -1,10 +1,55 @@
 import { Test } from '@nestjs/testing';
 import { CrudQueryService } from './crud-query.service';
+// import { mock } from 'jest-mock-extended';
 
-describe('TypeormService', () => {
+describe('TypeOrmService', () => {
+  let crudQueryService: CrudQueryService;
+
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       providers: [CrudQueryService],
     }).compile();
+
+    crudQueryService = moduleRef.get<CrudQueryService>(CrudQueryService);
+  });
+
+  describe('IsDefined', () => {
+    it('was CrudQueryService defined', async () => {
+      expect(crudQueryService).toBeDefined();
+    });
+  });
+
+  describe('modifyRequest', () => {
+    describe('when adding options', () => {
+      // it('should add options', async () => {
+      //   const req = {
+      //     options: {},
+      //   };
+      //   const options = {
+      //     search: '',
+      //   };
+      //   crudQueryService.modifyRequest(req, options);
+      //   expect(req.options).toEqual(options);
+      // });
+      const addOption = jest.fn().mockReturnValue({
+        addOptions: jest.fn().mockReturnValue({
+          query: { join: { company: { eager: false } } },
+        }),
+      });
+
+      // crudQueryService.modifyRequest(req, options);
+    });
+
+    describe('when adding search', () => {
+      it('should add search', () => {
+        const addSearch = jest.fn().mockReturnValue({
+          addSearch: jest.fn().mockReturnValue({
+            name: {
+              $eq: 'apple',
+            },
+          }),
+        });
+      });
+    });
   });
 });
