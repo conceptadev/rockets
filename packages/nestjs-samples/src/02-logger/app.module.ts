3776c2712f1e3ac08a4f84fe612e7c24917be9d1
Marshall Sorenson <marshall@conceptsol.com>
2022-02-03 18:31:19 +0000

    chore: linting

--- packages/nestjs-samples/src/02-logger/app.module.ts
+++ packages/nestjs-samples/src/02-logger/app.module.ts
@@ -3,7 +3,5 @@ import { OrderModule } from './order/order.module';
 import { LoggerModule } from '@rockts-org/nestjs-logger';
 @Module({
   imports: [LoggerModule.register(), OrderModule],
-  // TODO: transform loggerModule to work as a forRoot today is only working as forFeature
-  //imports:[LoggerModule.forRoot(), OrderModule],
 })
 export class AppModule {}
--- packages/nestjs-samples/src/02-logger/main.ts
+++ packages/nestjs-samples/src/02-logger/main.ts
@@ -11,9 +11,6 @@ async function bootstrap() {
   // Get reference of LoggerService From LoggerModule
   const customLoggerService = app.get(LoggerService);
 
-  // Inform that sentry transport will also be used
-  //customLoggerService.addTransport(loggerSentryTransport);
-
   // This is to inform that this logger will new used internally
   // or it will be used once yuo do a new Logger()
   app.useLogger(customLoggerService);
