67ca6f06e3ac6dd8ee0b4f559638cc6a930ae6ae
Marshall Sorenson <marshall.sorenson@gmail.com>
2022-03-29 15:28:03 +0000

    refactor: re-organize common interfaces

--- packages/nestjs-common/src/index.ts
+++ packages/nestjs-common/src/index.ts
@@ -3,13 +3,13 @@ export {
   createConfigurableDynamicRootModule,
 } from '@golevelup/nestjs-modules';
 
-export { IdentityInterface } from './interfaces/identity.interface';
-export { IdentityEmailInterface } from './interfaces/identity-email.interface';
-export { IdentityUsernameInterface } from './interfaces/identity-username.interface';
-export { OptionsInterface } from './interfaces/options.interface';
-export { ModuleOptionsControllerInterface } from './interfaces/module-options-controller.interface';
-export { ModuleOptionsSettingsInterface } from './interfaces/module-options-settings.interface';
-export { DeferExternalOptionsInterface } from './interfaces/defer-external-options.interface';
+export { IdentityInterface } from './interfaces/identity/identity.interface';
+export { IdentityEmailInterface } from './interfaces/identity/identity-email.interface';
+export { IdentityUsernameInterface } from './interfaces/identity/identity-username.interface';
+export { OptionsInterface } from './interfaces/options/options.interface';
+export { ModuleOptionsControllerInterface } from './interfaces/modules/module-options-controller.interface';
+export { ModuleOptionsSettingsInterface } from './interfaces/modules/module-options-settings.interface';
+export { DeferExternalOptionsInterface } from './interfaces/modules/defer-external-options.interface';
 
 export { deferExternal } from './modules/defer-external';
 export { negotiateController } from './modules/negotiate-controller';
--- packages/nestjs-common/src/interfaces/modules/module-options-controller.interface.ts
+++ packages/nestjs-common/src/interfaces/modules/module-options-controller.interface.ts
@@ -1,5 +1,5 @@
 import { Type } from '@nestjs/common';
-import { OptionsInterface } from './options.interface';
+import { OptionsInterface } from '../options/options.interface';
 
 export interface ModuleOptionsControllerInterface extends OptionsInterface {
   controller?: false | Type | Type[];
