d95356b60066100b6f5acb5b20fb2e880464faf6
leoafarias <leoafarias@gmail.com>
2022-04-25 09:21:31 -0400

    Code snippet improvements

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,9 +1,5 @@
-(typeof global !== 'undefined' ? global : window).Prism = Prism;
-import Prism from 'prism-react-renderer/prism';
 import React from 'react';
 import '../global.css';
-
-require('prismjs/components/prism-typescript');
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
