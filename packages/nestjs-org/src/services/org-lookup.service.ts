e03be3228b1182ea5d5a8a0deb552b717af220cd
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-05-31 15:26:13 +0000

    refactor: owner lookup service is required

--- packages/nestjs-org/src/services/org-lookup.service.ts
+++ packages/nestjs-org/src/services/org-lookup.service.ts
@@ -1,10 +1,6 @@
 import { FindConditions, Repository } from 'typeorm';
 import { Inject, Injectable } from '@nestjs/common';
-import {
-  LookupIdInterface,
-  ReferenceId,
-  ReferenceIdInterface,
-} from '@concepta/ts-core';
+import { ReferenceId, ReferenceIdInterface } from '@concepta/ts-core';
 import { InjectDynamicRepository } from '@concepta/nestjs-typeorm-ext';
 import { ReferenceLookupException } from '@concepta/typeorm-common';
 import {
@@ -14,6 +10,7 @@ import {
 import { OrgEntityInterface } from '../interfaces/org-entity.interface';
 import { OrgLookupServiceInterface } from '../interfaces/org-lookup-service.interface';
 import { OrgOwnerInterface } from '../interfaces/org-owner.interface';
+import { OrgOwnerLookupServiceInterface } from '../interfaces/org-owner-lookup-service.interface';
 
 /**
  * Org lookup service
@@ -29,7 +26,7 @@ export class OrgLookupService implements OrgLookupServiceInterface {
     @InjectDynamicRepository(ORG_MODULE_ORG_ENTITY_KEY)
     private orgRepo: Repository<OrgEntityInterface>,
     @Inject(ORG_MODULE_OWNER_LOOKUP_SERVICE)
-    private ownerLookupService?: LookupIdInterface,
+    private ownerLookupService: OrgOwnerLookupServiceInterface,
   ) {}
 
   /**
