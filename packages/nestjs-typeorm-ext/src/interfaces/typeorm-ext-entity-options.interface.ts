d3743cf13c8feb51e017da2f834db335673ae0e7
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-01 20:43:06 +0000

    refactor: adjust TypeOrmExtModule.forFeature signature

--- packages/nestjs-typeorm-ext/src/interfaces/typeorm-ext-entity-options.interface.ts
+++ packages/nestjs-typeorm-ext/src/interfaces/typeorm-ext-entity-options.interface.ts
@@ -2,7 +2,7 @@ import { EntitySchema, Repository } from 'typeorm';
 import { Type } from '@nestjs/common';
 import { TypeOrmExtConnectionToken } from '../typeorm-ext.types';
 
-export interface TypeOrmExtEntityOptionInterface<T> {
+export interface TypeOrmExtEntityOptionInterface<T = unknown> {
   entity: Type<T> | EntitySchema<T>;
   repository?: Type<Repository<T>>;
   connection?: TypeOrmExtConnectionToken;
