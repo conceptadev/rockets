89dea88c13b103b193d262150f7aca55c2cb67cc
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-03 22:19:10 +0000

    refactor: typeorm ext supports custom connection properly

--- packages/nestjs-typeorm-ext/src/utils/create-entity-repository-provider.ts
+++ packages/nestjs-typeorm-ext/src/utils/create-entity-repository-provider.ts
@@ -1,21 +1,25 @@
 import { Provider, Type } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { EntitySchema } from 'typeorm';
+import { TypeOrmExtConnectionToken } from '../typeorm-ext.types';
 import { getEntityRepositoryToken } from './get-entity-repository-token';
+import { TYPEORM_EXT_MODULE_DEFAULT_CONNECTION_NAME } from '../typeorm-ext.constants';
 
 /**
  * Create an entity repository provider function
  *
  * @param key Entity key
  * @param entity The entity
+ * @param connection The connection
  * @returns {Provider} Repository provider
  */
 export function createEntityRepositoryProvider<T>(
   key: string,
   entity: Type<T> | EntitySchema,
+  connection: TypeOrmExtConnectionToken = TYPEORM_EXT_MODULE_DEFAULT_CONNECTION_NAME,
 ): Provider {
   return {
     provide: getEntityRepositoryToken(key),
-    useExisting: getRepositoryToken(entity),
+    useExisting: getRepositoryToken(entity, connection),
   };
 }
