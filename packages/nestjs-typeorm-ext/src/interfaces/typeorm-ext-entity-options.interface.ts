812f41c9565ec4cc33a95cd94b5674b8441bb0ce
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-07 18:01:19 +0000

    refactor: type adjustment

--- packages/nestjs-typeorm-ext/src/interfaces/typeorm-ext-entity-options.interface.ts
+++ packages/nestjs-typeorm-ext/src/interfaces/typeorm-ext-entity-options.interface.ts
@@ -1,8 +1,11 @@
 import { EntitySchema, Repository } from 'typeorm';
 import { Type } from '@nestjs/common';
 import { TypeOrmExtConnectionToken } from '../typeorm-ext.types';
+import { ReferenceIdInterface } from '@concepta/ts-core';
 
-export interface TypeOrmExtEntityOptionInterface<T = unknown> {
+export interface TypeOrmExtEntityOptionInterface<
+  T extends ReferenceIdInterface = ReferenceIdInterface,
+> {
   entity: Type<T> | EntitySchema<T>;
   repository?: Type<Repository<T>>;
   connection?: TypeOrmExtConnectionToken;
