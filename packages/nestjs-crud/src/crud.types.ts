583f575b288aa16bc35ec545e80f0053cc9470c2
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-16 15:53:59 +0000

    refactor: allow additional crud method args

--- packages/nestjs-crud/src/crud.types.ts
+++ packages/nestjs-crud/src/crud.types.ts
@@ -9,3 +9,5 @@ export type DecoratorTargetObject<T extends Object = Object> = Type<T> | T;
 /* eslint-disable-next-line @typescript-eslint/ban-types */
 export type ReflectionTargetOrHandler = Function | Type<Object>;
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export type AdditionalCrudMethodArgs = any[];
--- packages/nestjs-crud/src/interfaces/crud-controller.interface.ts
+++ packages/nestjs-crud/src/interfaces/crud-controller.interface.ts
@@ -1,8 +1,9 @@
+import { DeepPartial } from 'typeorm';
 import { CreateManyDto } from '@nestjsx/crud';
 import { ReferenceIdInterface } from '@concepta/ts-core';
 import { CrudRequestInterface } from '../interfaces/crud-request.interface';
 import { CrudResponsePaginatedInterface } from './crud-response-paginated.interface';
-import { DeepPartial } from 'typeorm';
+import { AdditionalCrudMethodArgs } from '../crud.types';
 
 export interface CrudControllerInterface<
   Entity extends ReferenceIdInterface,
@@ -12,31 +13,45 @@ export interface CrudControllerInterface<
 > {
   getMany?: (
     crudRequest: CrudRequestInterface,
+    ...rest: AdditionalCrudMethodArgs
   ) => Promise<CrudResponsePaginatedInterface<Entity> | Entity[]>;
 
-  getOne?: (crudRequest: CrudRequestInterface) => Promise<Entity>;
+  getOne?: (
+    crudRequest: CrudRequestInterface,
+    ...rest: AdditionalCrudMethodArgs
+  ) => Promise<Entity>;
 
   createOne?: (
     crudRequest: CrudRequestInterface,
     dto: Creatable,
+    ...rest: AdditionalCrudMethodArgs
   ) => Promise<Entity>;
 
   createMany?: (
     crudRequest: CrudRequestInterface,
     dto: CreateManyDto<Creatable>,
+    ...rest: AdditionalCrudMethodArgs
   ) => Promise<Entity[]>;
 
   updateOne?: (
     crudRequest: CrudRequestInterface,
     dto: Updatable,
+    ...rest: AdditionalCrudMethodArgs
   ) => Promise<Entity>;
 
   replaceOne?: (
     crudRequest: CrudRequestInterface,
     dto: Replaceable,
+    ...rest: AdditionalCrudMethodArgs
   ) => Promise<Entity>;
 
-  deleteOne?: (crudRequest: CrudRequestInterface) => Promise<Entity | void>;
+  deleteOne?: (
+    crudRequest: CrudRequestInterface,
+    ...rest: AdditionalCrudMethodArgs
+  ) => Promise<Entity | void>;
 
-  recoverOne?: (crudRequest: CrudRequestInterface) => Promise<Entity>;
+  recoverOne?: (
+    crudRequest: CrudRequestInterface,
+    ...rest: AdditionalCrudMethodArgs
+  ) => Promise<Entity>;
 }
