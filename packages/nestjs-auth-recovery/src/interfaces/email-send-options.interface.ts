5a0e53bfc010012a928c927fd3f561dc7f80b3cb
Leonardo Cartaxo <leo.cartaxo@gmail.com>
2022-06-25 11:34:22 -0300

    fix: fix send email-send-options.interface.ts

--- packages/nestjs-auth-recovery/src/interfaces/email-send-options.interface.ts
+++ packages/nestjs-auth-recovery/src/interfaces/email-send-options.interface.ts
@@ -1,10 +1,12 @@
 export interface EmailSendOptionsInterface {
-  to: string;
-  cc: string;
-  bcc: string;
-  from: string;
-  context: string;
-  subject: string;
-  template: string;
-  icalEvent: string;
+  to?: string;
+  cc?: string;
+  bcc?: string;
+  from?: string;
+  context?: {
+    [name: string]: never;
+  };
+  subject?: string;
+  template?: string;
+  icalEvent?: string;
 }
