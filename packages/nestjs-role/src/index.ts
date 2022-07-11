44fb82e87d3b29e6caa4ce2cce5ca63789266c04
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-07-11 13:17:55 +0000

    fix: add missing role module entity template exports

--- packages/nestjs-role/src/index.ts
+++ packages/nestjs-role/src/index.ts
@@ -2,6 +2,8 @@ export { RoleModule } from './role.module';
 
 export { RolePostgresEntity } from './entities/role-postgres.entity';
 export { RoleSqliteEntity } from './entities/role-sqlite.entity';
+export { RoleAssignmentPostgresEntity } from './entities/role-assignment-postgres.entity';
+export { RoleAssignmentSqliteEntity } from './entities/role-assignment-sqlite.entity';
 
 export { RoleService } from './services/role.service';
 export { RoleLookupService } from './services/role-lookup.service';
