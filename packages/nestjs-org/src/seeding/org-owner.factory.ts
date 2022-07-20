a7fb7eec64d2ea1b303f3c3595d0c4447830b1cd
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-07-20 21:22:07 +0000

    fix: no need to override org owner factory entity method

--- packages/nestjs-org/src/seeding/org-owner.factory.ts
+++ packages/nestjs-org/src/seeding/org-owner.factory.ts
@@ -4,11 +4,4 @@ import { ReferenceIdInterface } from '@concepta/ts-core';
 /**
  * Org Owner factory
  */
-export class OrgOwnerFactory extends Factory<ReferenceIdInterface> {
-  /**
-   * Entity generator
-   */
-  protected async entity(): Promise<ReferenceIdInterface> {
-    throw new Error('Not implemented');
-  }
-}
+export class OrgOwnerFactory extends Factory<ReferenceIdInterface> {}
