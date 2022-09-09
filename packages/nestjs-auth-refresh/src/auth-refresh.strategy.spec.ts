00012aea60deb1ccd4f6263c07282d7d859d43ed
Thiago Ramalho <thiago.nramalho@gmail.com>
2022-09-09 20:31:45 -0300

    test: add test to  auth-refresh.strategy.ts

---
+++ packages/nestjs-auth-refresh/src/auth-refresh.strategy.spec.ts
@@ -0,0 +1,68 @@
+import { AuthRefreshStrategy } from './auth-refresh.strategy';
+import { AuthRefreshSettingsInterface } from './interfaces/auth-refresh-settings.interface';
+import { mock } from 'jest-mock-extended';
+import { VerifyTokenServiceInterface } from '@concepta/nestjs-authentication';
+import { AuthRefreshUserLookupServiceInterface } from './interfaces/auth-refresh-user-lookup-service.interface';
+import { randomUUID } from 'crypto';
+import { UserFixture } from './__fixtures__/user/user.entity.fixture';
+import { AuthorizationPayloadInterface } from '@concepta/ts-common';
+import { UnauthorizedException } from '@nestjs/common';
+
+describe(AuthRefreshStrategy, () => {
+  const USERNAME = 'username';
+
+  let user: UserFixture;
+  let settings: Partial<AuthRefreshSettingsInterface>;
+  let userLookUpService: AuthRefreshUserLookupServiceInterface;
+  let verifyTokenService: VerifyTokenServiceInterface;
+  let authRefreshStrategy: AuthRefreshStrategy;
+  let authorizationPayloadInterface: AuthorizationPayloadInterface;
+
+  beforeEach(async () => {
+    settings = mock<Partial<AuthRefreshSettingsInterface>>();
+
+    userLookUpService = mock<AuthRefreshUserLookupServiceInterface>();
+    verifyTokenService = mock<VerifyTokenServiceInterface>();
+    authRefreshStrategy = new AuthRefreshStrategy(
+      settings,
+      verifyTokenService,
+      userLookUpService,
+    );
+
+    user = new UserFixture();
+    user.id = randomUUID();
+
+    authorizationPayloadInterface = {
+      sub: USERNAME,
+    };
+
+    jest.spyOn(userLookUpService, 'bySubject').mockResolvedValue(user);
+  });
+
+  it('constructor', async () => {
+    settings = mock<Partial<AuthRefreshSettingsInterface>>();
+    authRefreshStrategy = new AuthRefreshStrategy(
+      settings,
+      verifyTokenService,
+      userLookUpService,
+    );
+    expect(true).toBeTruthy();
+  });
+
+  describe(AuthRefreshStrategy.prototype.validate, () => {
+    it('should return user', async () => {
+      const result = await authRefreshStrategy.validate(
+        authorizationPayloadInterface,
+      );
+      expect(result.id).toBe(user.id);
+    });
+
+    it(`should return no user on ${userLookUpService.bySubject}`, async () => {
+      jest.spyOn(userLookUpService, 'bySubject').mockResolvedValue(null);
+
+      const t = () =>
+        authRefreshStrategy.validate(authorizationPayloadInterface);
+      expect(t).rejects.toThrow(UnauthorizedException);
+    });
+  });
+});
