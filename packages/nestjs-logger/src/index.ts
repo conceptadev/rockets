e673a17ffdf778e50643899593d2a16c9020cfb4
Kauan Guesser <contato@kauan.net>
2023-09-27 17:24:03 -0300

    feat: export logger interfaces

--- packages/nestjs-logger/src/index.ts
+++ packages/nestjs-logger/src/index.ts
@@ -4,3 +4,5 @@ export * from './logger-exception.filter';
 export * from './logger-request.interceptor';
 export * from './logger-transport.service';
 export * from './transports/logger-sentry.transport';
+export * from './interfaces/logger-settings.interface';
+export * from './interfaces/logger-sentry-config.interface';
