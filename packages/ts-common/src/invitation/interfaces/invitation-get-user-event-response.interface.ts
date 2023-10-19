a6bc5a48505ccbe13a5d9dbcce4e32cac2cdd4d5
Marshall Sorenson <mashall.sorenson@gmail.com>
2023-10-19 10:35:21 -0400

    fix(invitation): get user response does not need username

--- packages/ts-common/src/invitation/interfaces/invitation-get-user-event-response.interface.ts
+++ packages/ts-common/src/invitation/interfaces/invitation-get-user-event-response.interface.ts
@@ -1,10 +1,8 @@
 import {
   ReferenceEmailInterface,
   ReferenceIdInterface,
-  ReferenceUsernameInterface,
 } from '@concepta/ts-core';
 
 export interface InvitationGetUserEventResponseInterface
   extends ReferenceIdInterface,
-    ReferenceUsernameInterface,
     ReferenceEmailInterface {}
