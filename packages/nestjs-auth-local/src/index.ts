0b435d1d21225c9b8b94a47d6e8d636d6d6d178f
Marshall Sorenson <marshall.sorenson@gmail.com>
2024-06-12 10:35:11 -0400

    feat: local-auth doc improvements

--- packages/nestjs-auth-local/src/index.ts
+++ packages/nestjs-auth-local/src/index.ts
@@ -1,10 +1,17 @@
+// interfaces
 export { AuthLocalValidateUserInterface } from './interfaces/auth-local-validate-user.interface';
 export { AuthLocalUserLookupServiceInterface } from './interfaces/auth-local-user-lookup-service.interface';
 export { AuthLocalValidateUserServiceInterface } from './interfaces/auth-local-validate-user-service.interface';
+export { AuthLocalCredentialsInterface } from './interfaces/auth-local-credentials.interface';
 
-export * from './auth-local.module';
-export * from './auth-local.controller';
-export * from './dto/auth-local-login.dto';
+// DTOs
+export { AuthLocalLoginDto } from './dto/auth-local-login.dto';
+
+// module
+export { AuthLocalModule } from './auth-local.module';
+export { AuthLocalController } from './auth-local.controller';
+export { AuthLocalValidateUserService } from './services/auth-local-validate-user.service';
+
 export {
   AuthLocalGuard,
   AuthLocalGuard as LocalAuthGuard,
