6e8843be1f53cddbb2c8ba4b33b908a4074c64cd
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-06-27 17:27:29 -0300

    chore: cleanup non used functions

--- packages/nestjs-core/src/index.ts
+++ packages/nestjs-core/src/index.ts
@@ -1,11 +1,2 @@
-export {
-  AsyncModuleConfig,
-  createConfigurableDynamicRootModule,
-} from '@golevelup/nestjs-modules';
-
 export { ModuleOptionsControllerInterface } from './modules/interfaces/module-options-controller.interface';
 export { ModuleOptionsSettingsInterface } from './modules/interfaces/module-options-settings.interface';
-export { DeferExternalOptionsInterface } from './modules/interfaces/defer-external-options.interface';
-
-export { deferExternal } from './modules/defer-external';
-export { negotiateController } from './modules/negotiate-controller';
