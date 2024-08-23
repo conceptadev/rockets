8edd70258af0a895d61a0b7e768a33ea84bf9e31
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-08-23 12:26:41 -0300

    chore: add exports

--- packages/nestjs-auth-github/src/index.ts
+++ packages/nestjs-auth-github/src/index.ts
@@ -1,7 +1,19 @@
 export * from './auth-github.module';
 export * from './auth-github.controller';
+
+// dto
 export * from './dto/auth-github-login.dto';
+
+// types
 export * from './auth-github.types';
+
+// utils
+export * from './utils/auth-github-map-profile';
+
+// interfaces
+export * from './interfaces/auth-github-profile.interface';
+export * from './interfaces/auth-github-credentials.interface';
+
 
 export {
   AuthGithubGuard,
