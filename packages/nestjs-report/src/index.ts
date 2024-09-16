7bab55a22180d98fa2f28e7a1f22f31f3722dfda
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-09-16 17:26:10 -0300

    chore: add export

--- packages/nestjs-report/src/index.ts
+++ packages/nestjs-report/src/index.ts
@@ -3,6 +3,7 @@ export { ReportEntityInterface } from './interfaces/report-entity.interface';
 
 export { ReportServiceInterface } from './interfaces/report-service.interface';
 export { ReportGeneratorServiceInterface } from './interfaces/report-generator-service.interface';
+export { ReportGeneratorResultInterface } from './interfaces/report-generator-result.interface';
 
 export { ReportPostgresEntity } from './entities/report-postgres.entity';
 export { ReportSqliteEntity } from './entities/report-sqlite.entity';
