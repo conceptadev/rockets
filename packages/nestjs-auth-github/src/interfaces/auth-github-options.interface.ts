05e6e7b90bfbf2a750430ea89da27a0771cd53c1
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-09-25 17:30:09 -0300

    chore: add readme documentation

--- packages/nestjs-auth-github/src/interfaces/auth-github-options.interface.ts
+++ packages/nestjs-auth-github/src/interfaces/auth-github-options.interface.ts
@@ -5,7 +5,10 @@ import { AuthGithubSettingsInterface } from './auth-github-settings.interface';
 export interface AuthGithubOptionsInterface
   extends ModuleOptionsSettingsInterface<AuthGithubSettingsInterface> {
   /**
-   * Implementation of a class to issue tokens
+   * Implementation of a class to issue tokens, which is used as injection
+   * in the controller to generate the response payload, with access token
+   * and refresh token. If you overwrite the controller, the issueTokenService
+   * won't be used, even if you overwrite it.
    */
   issueTokenService?: IssueTokenServiceInterface;
 
