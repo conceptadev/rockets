f2eaba998707f7a4d6728cb9fa78aa0614df6a4f
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-09-30 17:41:40 -0300

    chore: linting

--- packages/nestjs-auth-apple/src/config/auth-apple-default.config.ts
+++ packages/nestjs-auth-apple/src/config/auth-apple-default.config.ts
@@ -15,12 +15,13 @@ export const authAppleDefaultConfig = registerAs(
      * The login dto
      */
     loginDto: AuthAppleLoginDto,
-    clientID: process.env.APPLE_CLIENT_ID ?? '',
-    callbackURL: process.env.APPLE_CALLBACK_URL ?? '',
-    teamID: process.env.APPLE_TEAM_ID ?? '',
-    keyID: process.env.APPLE_KEY_ID ?? '',
+    clientID: process.env.APPLE_CLIENT_ID ?? 'client_id',
+    callbackURL: process.env.APPLE_CALLBACK_URL ?? 'callback_url',
+    teamID: process.env.APPLE_TEAM_ID ?? 'teamID',
+    keyID: process.env.APPLE_KEY_ID ?? 'keyID',
     privateKeyLocation: process.env.APPLE_PRIVATE_KEY_LOCATION ?? '',
-    privateKeyString: process.env.APPLE_PRIVATE_KEY_STRING ?? '',
+    privateKeyString:
+      process.env.APPLE_PRIVATE_KEY_STRING ?? '',
     passReqToCallback: false,
     scope:
       'APPLE_SCOPE' in process.env && process.env.APPLE_SCOPE
