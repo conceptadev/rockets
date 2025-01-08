39487a962856e8509d50345689a60a58ed0e1788
Thiago Ramalho <thiago.nramalho@gmail.com>
2025-01-08 20:20:00 -0300

    chore: add logo to all email itemplates

--- packages/nestjs-auth-recovery/src/services/auth-recovery-notification.service.ts
+++ packages/nestjs-auth-recovery/src/services/auth-recovery-notification.service.ts
@@ -48,26 +48,30 @@ export class AuthRecoveryNotificationService
     });
   }
 
-  async sendPasswordUpdatedSuccefullyEmail(email: string): Promise<void> {
-    const { from } = this.settings.email;
-    const { subject, fileName } = this.settings.email.templates.passwordUpdated;
-    await this.sendEmail({
-      from,
-      subject,
-      to: email,
-      template: fileName,
-    });
-  }
-
-  async sendRecoverLoginEmail(email: string, username: string): Promise<void> {
-    const { from } = this.settings.email;
-    const { subject, fileName } = this.settings.email.templates.recoverLogin;
+  async sendPasswordUpdatedSuccessfullyEmail(email: string): Promise<void> {
+    const { from, baseUrl} = this.settings.email;
+    const { subject, fileName, logo } = this.settings.email.templates.passwordUpdated;
     await this.sendEmail({
       from,
       subject,
       to: email,
       template: fileName,
       context: {
+        logo: `${baseUrl}/${logo}`,
+      },
+    });
+  }
+
+  async sendRecoverLoginEmail(email: string, username: string): Promise<void> {
+    const { from, baseUrl } = this.settings.email;
+    const { subject, fileName, logo } = this.settings.email.templates.recoverLogin;
+    await this.sendEmail({
+      from,
+      subject,
+      to: email,
+      template: fileName,
+      context: {
+        logo: `${baseUrl}/${logo}`,
         login: username,
       },
     });
