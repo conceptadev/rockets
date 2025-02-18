07279f3280e5244bbc0a7c663f2beeae630d1730
Thiago Ramalho <thiago.nramalho@gmail.com>
2025-02-18 19:55:03 -0300

    chore: extends ReferenceAssigneeInterface

--- packages/nestjs-role/src/interfaces/assignment-context.ts
+++ packages/nestjs-role/src/interfaces/assignment-context.ts
@@ -1,9 +1,10 @@
 import {
+  ReferenceAssigneeInterface,
   ReferenceAssignment,
   ReferenceIdInterface,
 } from '@concepta/nestjs-common';
 
-export interface AssignmentContext<T extends ReferenceIdInterface> {
+export interface AssignmentContext<T extends ReferenceIdInterface>
+  extends ReferenceAssigneeInterface<T> {
   assignment: ReferenceAssignment;
-  assignee: T;
 }
