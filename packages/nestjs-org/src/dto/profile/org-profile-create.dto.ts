49ff40831a91f95f0711e535b73c13994f163a93
Marshall Sorenson <marshall.sorenson@gmail.com>
2025-02-25 16:38:29 -0500

    chore: linting

--- packages/nestjs-org/src/dto/profile/org-profile-create.dto.ts
+++ packages/nestjs-org/src/dto/profile/org-profile-create.dto.ts
@@ -1,5 +1,5 @@
 import { Exclude } from 'class-transformer';
-import { IntersectionType, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { OrgProfileCreatableInterface } from '@concepta/nestjs-common';
 import { OrgProfileDto } from './org-profile.dto';
 
