ed3580af1baa3bca7087f9394353ae08eb6c8ede
Marshall Sorenson <marshall.sorenson@gmail.com>
2025-03-18 08:33:53 -0400

    chore: clean up file name

--- packages/nestjs-typeorm-ext/src/index.ts
+++ packages/nestjs-typeorm-ext/src/index.ts
@@ -4,7 +4,7 @@ export { TypeOrmExtOptions } from './typeorm-ext.types';
 export { TypeOrmExtEntityOptionInterface } from './interfaces/typeorm-ext-entity-options.interface';
 
 export { InjectEntityRepository } from './decorators/inject-entity-repository.decorator';
-export { InjectDynamicRepository } from './decorators/inject-dymamic-repository.decorator';
+export { InjectDynamicRepository } from './decorators/inject-dynamic-repository.decorator';
 
 export { getEntityRepositoryToken } from './utils/get-entity-repository-token';
 export { getDynamicRepositoryToken } from './utils/get-dynamic-repository-token';
