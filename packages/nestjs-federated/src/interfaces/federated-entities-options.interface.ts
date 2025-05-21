87f06dbe9e0b74ebec6824c9479ab96da623436b
Marshall Sorenson <marshall.sorenson@gmail.com>
2025-05-21 16:09:18 -0400

    chore: linting

--- packages/nestjs-federated/src/interfaces/federated-entities-options.interface.ts
+++ packages/nestjs-federated/src/interfaces/federated-entities-options.interface.ts
@@ -1,7 +1,9 @@
-import { TypeOrmExtEntityOptionInterface } from '@concepta/nestjs-typeorm-ext';
+import {
+  FederatedEntityInterface,
+  RepositoryEntityOptionInterface,
+} from '@concepta/nestjs-common';
 import { FEDERATED_MODULE_FEDERATED_ENTITY_KEY } from '../federated.constants';
-import { FederatedEntityInterface } from '@concepta/nestjs-common';
 
 export interface FederatedEntitiesOptionsInterface {
-  [FEDERATED_MODULE_FEDERATED_ENTITY_KEY]: TypeOrmExtEntityOptionInterface<FederatedEntityInterface>;
+  [FEDERATED_MODULE_FEDERATED_ENTITY_KEY]: RepositoryEntityOptionInterface<FederatedEntityInterface>;
 }
